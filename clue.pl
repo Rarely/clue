a9fdfc96a01005f1116d4bd03d5f0a0fa0471287
Cody <codyjrobinson@hotmail.com>
2013-11-19 15:10:47 -0800

    Opponents turns
    Added opponents turns  and the ability to ask them:
    1. Did the opponent win
    2. Did they make a suggestion
    3. Record the suggestion.
    
    Still need to give the possible cards a weighted value.

--- clue.pl
+++ clue.pl
@@ -19,6 +19,10 @@ clue :- init.
 :- dynamic knownCharacters/1.
 :- dynamic knownRooms/1.
 
+:- dynamic possibleWeapons/2.
+:- dynamic possibleCharacters/2.
+:- dynamic possibleRooms/2.
+
 % Pieces and Cards %
 
 % List of all possible characters %
@@ -92,11 +96,19 @@ set_character(Card) :- assert(knownCharacters(Card)),
 set_room(Card) :- assert(knownRooms(Card)),
 				  retract(unknownRooms(Card)).
 
+set_possible(Card) :- weapon(Card),
+				      assert(possibleWeapons(Card)).
+
+set_possible(Card) :- character(Card),
+				      assert(possibleCharacters(Card)).
+
+set_possible(Card) :- room(Card),
+				      assert(possibleRooms(Card)).
 
 %------ Initalize all cards to unknown ------%
 init_game_state :- init_characters, 
-				  init_weapons, 
-				  init_rooms.
+				   init_weapons, 
+				   init_rooms.
 
 % Initalize all characters to unknown %
 init_characters :- assert(unknownCharacters(scarlett)),
@@ -125,9 +137,49 @@ init_rooms :- assert(unknownRooms(kitchen)),
 			  assert(unknownRooms(hall)),
 			  assert(unknownRooms(study)).
 
-clear_game_state :- retractall(unknownWeapons(_)),
+clear_game_state :- retractall(numPlayers(_)),
+					retractall(unknownWeapons(_)),
 					retractall(unknownCharacters(_)),
 					retractall(unknownRooms(_)),
 					retractall(knownWeapons(_)),
 					retractall(knownCharacters(_)),
-					retractall(knownRooms(_)).
+					retractall(knownRooms(_)),
+					retractall(possibleWeapons(_)),
+					retractall(possibleCharacters(_)),
+					retractall(possibleRooms(_)).
+
+
+
+
+
+%------ Opponents turns ------%
+opponents_turn(X) :- numPlayers(X), % Its now your turn again %
+					 write('\nIt is now your turn to play again!').
+opponents_turn(X) :- write('\n\nIt is the '), write(X), write(' opponent\'s turn\n'),
+					 Z is X + 1,
+					 opponent_win,
+				     opponent_ask,
+				     opponents_turn(Z). 
+
+opponent_win :- write('Did your opponent win?\n(y/n)'),
+				read(Win),
+				game_over(Win).
+
+opponent_ask :- write('Did your opponent make a guess?\n(y/n)'),
+				read(Guess),
+				opponent_guess(Guess).
+
+opponent_guess(n).
+
+opponent_guess(y) :- write('What was one of you opponent\'s guesses?\n'),
+					 read(Guess),
+					 set_possible(Guess),
+					 write('What was their other guess? \n'),
+					 read(Guess2),
+					 set_possible(Guess2),
+					 write('What was their last guess? \n'),
+					 read(Guess3),
+					 set_possible(Guess3).
+
+game_over(n).
+game_over(y):- write('That sucks! \nBetter luck next time. \nPlease don\'t blame me.'), false.
