4488fa1040cbad1b4a67dcc93163d56d14ba223e
Cody <codyjrobinson@hotmail.com>
2013-11-21 21:57:40 -0800

    Refactor!

--- clue.pl
+++ clue.pl
@@ -2,33 +2,27 @@
 
 % TODO LIST %
 % Show a card when opponent guesses 
-% Refactor Possible into Unknown, they do the same thing! 
-% Beginning known cards to loop for only a known number 
 % Error handling: Loop Exit and Re-entry
 	     	    % invalid entries
 
-% First call to start the Clue Helper %
+% First call to start the Clue Helper 
 clue :- init.
 
-% The dynamic lists the will be changing throughout the game %
+% The dynamic lists the will be changing throughout the game 
 :- dynamic numPlayers/1.
 :- dynamic currentRoom/1.
 
-:- dynamic unknownWeapons/1.
-:- dynamic unknownCharacters/1.
-:- dynamic unknownRooms/1.
+:- dynamic unknownWeapons/2.
+:- dynamic unknownCharacters/2.
+:- dynamic unknownRooms/2.
 
 :- dynamic knownWeapons/1.
 :- dynamic knownCharacters/1.
 :- dynamic knownRooms/1.
 
-:- dynamic possibleWeapons/2.
-:- dynamic possibleCharacters/2.
-:- dynamic possibleRooms/2.
+%------ Pieces and Cards ------%
 
-% Pieces and Cards %
-
-% List of all possible characters %
+% List of all possible characters
 character(scarlett).
 character(mustard).
 character(white).
@@ -36,7 +30,7 @@ character(green).
 character(peacock).
 character(plum).
 
-% List of all possible weapons %
+% List of all possible weapons
 weapon(candlestick).
 weapon(dagger).
 weapon(pipe).
@@ -44,7 +38,7 @@ weapon(revolver).
 weapon(rope).
 weapon(wrench).
 
-% List of all possible rooms %
+% List of all possible rooms
 room(kitchen).
 room(ballroom).
 room(conservatory).
@@ -55,22 +49,64 @@ room(lounge).
 room(hall).
 room(study).
 
-% Initalize the game state %
+% Initalize the game state
 init :- clear_game_state,
 		init_game_state,
 		get_num_players,
 		get_cards.
 
-% Get the number of players playing %
+%------ Initalize all cards to unknown ------%
+init_game_state :- init_characters, 
+				   init_weapons, 
+				   init_rooms.
+
+% Initalize all characters to unknown
+init_characters :- assert(unknownCharacters(scarlett, 0)),
+				   assert(unknownCharacters(mustard, 0)),
+				   assert(unknownCharacters(white, 0)),
+				   assert(unknownCharacters(green, 0)),
+				   assert(unknownCharacters(peacock, 0)),
+				   assert(unknownCharacters(plum, 0)).
+
+% Initalize all weapons to unknown 
+init_weapons :- assert(unknownWeapons(candlestick, 0)),
+				assert(unknownWeapons(dagger, 0)),
+				assert(unknownWeapons(pipe, 0)),
+				assert(unknownWeapons(revolver, 0)),
+				assert(unknownWeapons(rope, 0)),
+				assert(unknownWeapons(wrench, 0)).
+
+% Initalize all rooms to unknown
+init_rooms :- assert(unknownRooms(kitchen, 0)),
+			  assert(unknownRooms(ballroom, 0)),
+			  assert(unknownRooms(conservatory, 0)),
+			  assert(unknownRooms(diningroom, 0)),
+			  assert(unknownRooms(billiard, 0)),
+			  assert(unknownRooms(library, 0)),
+			  assert(unknownRooms(lounge, 0)),
+			  assert(unknownRooms(hall, 0)),
+			  assert(unknownRooms(study, 0)).
+
+% Clear the game state
+clear_game_state :- retractall(numPlayers(_)),
+					retractall(knownCharacters(_)),
+					retractall(knownWeapons(_)),
+					retractall(knownRooms(_)),
+					retractall(unknownCharacters(_,_)),
+					retractall(unknownWeapons(_,_)),
+					retractall(unknownRooms(_,_)).
+
+%------ Start of Game information ------%
+
+% Get the number of players playing 
 get_num_players :- write('How many players are there? Example "6." \n'),
 				   read(NumPlayers),
 				   set_players(NumPlayers).
 
-% Set the number of players playing %
+% Set the number of players playing
 set_players(NumPlayers) :- assert(numPlayers(NumPlayers)).
 
-
-% Get all the cards player is holder %
+% Get all the cards player is holder
 get_cards :- write('Please state the cards you are holding.\n Example "candlestick.", if you have entered all cards type "done." \n' ),
 			 read(Card),
 			 set_card(Card).
@@ -86,133 +122,59 @@ is_our_turn(n) :- write('Which player\'s turn, from your left, is it?\n(0..Numbe
                   read(OpponentsTurn),
                   opponents_turn(OpponentsTurn).
              
-%------ Check to see the type of card or if we are done ------%
-set_card(done) :- !, find_turn.
+%------ Update Cards ------%
 
-set_card(Card) :- weapon(Card),
-				   set_weapon(Card),
-				   get_cards.
+update_card(done) :- find_turn.
 
-set_card(Card) :- character(Card),
-				   set_character(Card),
-				   get_cards.
+update_card(Card) :- character(Card),
+				   	 assert(knownCharacters(Card)),
+				     retractall(unknownCharacters(Card,_)),
+				   	 get_cards.
 
-set_card(Card) :- room(Card),
-				   set_room(Card),
-				   get_cards.				   
+update_card(Card) :- weapon(Card),
+				   	 assert(knownWeapons(Card)),
+				     retractall(unknownWeapons(Card,_)),
+				   	 get_cards.
 
-set_weapon(Card) :- assert(knownWeapons(Card)),
-				    retractall(unknownWeapons(Card)),
-				    retractall(possibleWeapons(Card,_)).
+update_card(Card) :- room(Card),
+				   	 assert(knownRooms(Card)),
+				  	 retractall(unknownRooms(Card,_)),
+				   	 get_cards.				   
 
-set_character(Card) :- assert(knownCharacters(Card)),
-				       retractall(unknownCharacters(Card)),
-				       retractall(possibleCharacters(Card,_)).
+update_unknown(Card) :- knownCharacters(Card).
+update_unknown(Card) :- knownWeapons(Card). 
+update_unknown(Card) :- knownRooms(Card).
 
-set_room(Card) :- assert(knownRooms(Card)),
-				  retractall(unknownRooms(Card)),
-				  retractall(possibleRooms(Card,_)).
+update_unknown(Card) :- character(Card),
+					  	unknownCharacters(Card),
+				      	update_unknown_character(Card).
 
-set_possible(Card) :- knownCharacters(Card).
-set_possible(Card) :- knownRooms(Card).
-set_possible(Card) :- knownWeapons(Card). 
+update_unknown(Card) :- weapon(Card),
+					 	unknownWeapons(Card),
+					  	update_unknown_weapon(Card).
 
-set_possible(Card) :- weapon(Card),
-					  unknownWeapons(Card),
-					  set_possible_weapon(Card).
+update_unknown(Card) :- room(Card),
+					  	unknownRooms(Card),
+				      	update_unknown_room(Card).
 
-set_possible(Card) :- character(Card),
-					  unknownCharacters(Card),
-				      set_possible_character(Card).
-
-set_possible(Card) :- room(Card),
-					  unknownRooms(Card),
-				      set_possible_room(Card).
-
-%----- Set Possible Characters -----%
-set_possible_character(Card) :- possibleCharacters(Card,X),
-							    Y is X + 1,
-							    retractall(possibleCharacters(Card,_)),
-							    assert(possibleCharacters(Card, Y)).
-
-%----- Set Possible Rooms -----%
-set_possible_room(Card) :- possibleRooms(Card,X),
-						   Y is X + 1,
-						   retractall(possibleRooms(Card,_)),
-						   assert(possibleRooms(Card, Y)).
-
-%----- Set Possible Weapons -----%
-set_possible_weapon(Card) :- possibleWeapons(Card,X),
+update_unknown_char(Card) :- unknownCharacters(Card,X),
 							 Y is X + 1,
-							 retractall(possibleWeapons(Card,_)),
-							 assert(possibleWeapons(Card, Y)).
+							 retractall(unknownCharacters(Card,_)),
+							 assert(unknownCharacters(Card, Y)).
 
+update_unknown_weap(Card) :- unknownWeapons(Card,X),
+							 Y is X + 1,
+							 retractall(unknownWeapons(Card,_)),
+							 assert(unknownWeapons(Card, Y)).
 
-%------ Initalize all cards to unknown ------%
-init_game_state :- init_characters, 
-				   init_weapons, 
-				   init_rooms.
+update_unknown_room(Card) :- unknownRooms(Card,X),
+						     Y is X + 1,
+						     retractall(unknownRooms(Card,_)),
+						     assert(unknownRooms(Card, Y)).
 
-% Initalize all characters to unknown and possible %
-init_characters :- assert(unknownCharacters(scarlett)),
-				   assert(unknownCharacters(mustard)),
-				   assert(unknownCharacters(white)),
-				   assert(unknownCharacters(green)),
-				   assert(unknownCharacters(peacock)),
-				   assert(unknownCharacters(plum)),
-                   assert(possibleCharacters(scarlett, 0)),
-				   assert(possibleCharacters(mustard, 0)),
-				   assert(possibleCharacters(white, 0)),
-				   assert(possibleCharacters(green, 0)),
-				   assert(possibleCharacters(peacock, 0)),
-				   assert(possibleCharacters(plum, 0)).
-
-% Initalize all weapons to unknown and possible %
-init_weapons :- assert(unknownWeapons(candlestick)),
-				assert(unknownWeapons(dagger)),
-				assert(unknownWeapons(pipe)),
-				assert(unknownWeapons(revolver)),
-				assert(unknownWeapons(rope)),
-				assert(unknownWeapons(wrench)),
-				assert(possibleWeapons(candlestick, 0)),
-				assert(possibleWeapons(dagger, 0)),
-				assert(possibleWeapons(pipe, 0)),
-				assert(possibleWeapons(revolver, 0)),
-				assert(possibleeapons(rope, 0)),
-				assert(possibleWeapons(wrench, 0)).
-
-% Initalize all rooms to unknown and possible %
-init_rooms :- assert(unknownRooms(kitchen)),
-			  assert(unknownRooms(ballroom)),
-			  assert(unknownRooms(conservatory)),
-			  assert(unknownRooms(diningroom)),
-			  assert(unknownRooms(billiard)),
-			  assert(unknownRooms(library)),
-			  assert(unknownRooms(lounge)),
-			  assert(unknownRooms(hall)),
-			  assert(unknownRooms(study)),
-              assert(possibleRooms(kitchen, 0)),
-			  assert(possibleRooms(ballroom, 0)),
-			  assert(possibleRooms(conservatory, 0)),
-			  assert(possibleRooms(diningroom, 0)),
-			  assert(possibleRooms(billiard, 0)),
-			  assert(possibleRooms(library, 0)),
-			  assert(possibleRooms(lounge, 0)),
-			  assert(possibleRooms(hall, 0)),
-			  assert(possibleRooms(study, 0)).
-
-clear_game_state :- retractall(numPlayers(_)),
-					retractall(unknownWeapons(_)),
-					retractall(unknownCharacters(_)),
-					retractall(unknownRooms(_)),
-					retractall(knownWeapons(_)),
-					retractall(knownCharacters(_)),
-					retractall(knownRooms(_)),
-					retractall(possibleWeapons(_,_)),
-					retractall(possibleCharacters(_,_)),
-					retractall(possibleRooms(_,_)).
 
 %------ Our turn ------%
+
 my_turn :- write('Are you in a room?\n(y/n)'),
            read(InRoom),
            in_room_handler(InRoom).
@@ -221,8 +183,8 @@ in_room_handler(n) :- closest_room_interface.
 in_room_handler(y) :- which_room_interface. 
 
 which_room_interface :- write('Which room? (Ex: hall.) \n'),
-                      read(Room),
-                      which_room_handler(Room).
+                        read(Room),
+                        which_room_handler(Room).
 
 which_room_handler(Room) :- knownRooms(Room), already_seen_room_interface.  
 which_room_handler(Room) :- unknownRooms(Room), exit_room_interface.
@@ -231,8 +193,8 @@ already_seen_room_interface :- write('We\'ve already seen this room \n'),
 							   closest_room_interface.
 
 closest_room_interface :- write('What is the closest room?\n'),
-                        read(ClosestRoom),
-                        closest_room_handler(ClosestRoom).
+                          read(ClosestRoom),
+                          closest_room_handler(ClosestRoom).
 
 closest_room_handler(ClosestRoom) :- knownRooms(ClosestRoom), !, loop_next_closest_room_interface.
 
@@ -289,18 +251,19 @@ print_guesses(X,Y,Z) :- write('Character: '), write(X), write('\n'),
 
 guess_room(Room) :- currentRoom(Room).
 
-guess_character(Char) :- findall(X, possibleCharacters(_,X),L),
-						 max(L,Max),
-						 possibleCharacters(Char,Max).
+guess_char(Char) :- findall(X, unknownCharacters(_,X),L),
+				    max(L,Max),
+					unknownCharacters(Char,Max).
 
-guess_weapon(Weapon) :- findall(X, possibleWeapons(_,X),L),
-						 max(L,Max),
-						 possibleWeapons(Weapon,Max).
+guess_weap(Weap) :- findall(X, unknownWeapons(_,X),L),
+					max(L,Max),
+					unknownWeapons(Weap,Max).
 max([X],X).
 max([X|Xs],X):- max(Xs,Y), X >=Y.
 max([X|Xs],N):- max(Xs,N), N > X.
 
 %------ Opponents turns ------%
+
 opponents_turn :- opponents_turn(0).
 
 opponents_turn(X) :- numPlayers(X), % Its now your turn again %
@@ -325,13 +288,16 @@ opponent_guess(n).
 
 opponent_guess(y) :- write('What was your opponent\'s character guess?\n'),
 					 read(Guess),
-					 set_possible(Guess),
+					 update_unknown(Guess),
 					 write('What was their room guess? \n'),
 					 read(Guess2),
-					 set_possible(Guess2),
+					 update_unknown(Guess2),
 					 write('What was their weapon guess? \n'),
 					 read(Guess3),
-					 set_possible(Guess3).
+					 update_unknown(Guess3).
 
 game_over(n).
 game_over(y):- write('That sucks! \nBetter luck next time. \nPlease don\'t blame me.'), fail.
+
+fact(0,1).
+fact(N,F) :- N > 0,N1 is N - 1, fact(N1,F1),F is F1 * N.
